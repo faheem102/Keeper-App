c695a818332b38a93747dd3b8ec2165620754d31
Faheem Hassan <faheemhassan1985@gmail.com>
2023-06-07 02:09:04 +0200

    Rending Notes using map function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,19 @@ import "./App.css";
 import Header from "./Components/Header";
 import { Note } from "./Components/Note";
 import { Footer } from "./Components/Footer";
+import notes from "./notes";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Note />
+      {notes.map((noteEntry) => (
+        <Note
+          key={noteEntry.id}
+          title={noteEntry.title}
+          content={noteEntry.content}
+        />
+      ))}
       <Footer />
     </div>
   );
